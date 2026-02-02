a7b719d096c0f4c92db84506b77d352ce0d95373
David <davidmontiel549@gmail.com>
2026-02-02 15:12:25 -0800

    commit changes

--- src/task1.py
+++ src/task1.py
@@ -9,6 +9,22 @@ def text_to_digest(input):
 
 
 def hamming_distance(str1, str2):
+    bytes_1 = str1.encode("utf-8")
+    bytes_2 = str2.encode("utf-8")
+    count_0 = 0
+    for byte1, byte2 in zip(bytes_1, bytes_2):
+        if bytes_1 != bytes_2:
+            count_0 += 1
+
+    print(f"Byte difference {count_0}")
+    digest_1 = sha256(bytes_1).digest()
+    digets_2 = sha256(bytes_2).digest()
+    digest_counter = 0
+    for byte1, byte2 in zip(digest_1, digets_2):
+        if bytes_1 != bytes_2:
+            digest_counter += 1
+
+    print(f"Byte difference Digest: {digest_counter}")
     count = 0
     for char1, char2 in zip(str1, str2):
         if char1 != char2:
@@ -51,6 +67,12 @@ def main():
             print("Provide an integer value please")
     else:
         print("Provide an arguement")
+    # Hash string who hamming distance is one
+
+    test_str1 = ["one", "two", "three", "four"]
+    test_str2 = ["ode", "too", "theee", "fotr"]
+    for word1, word2 in zip(test_str1, test_str2):
+        print(hamming_distance(word1, word2))
 
 
 print()
